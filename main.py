dfb9def56c175dbe830a0e9f98a379c35a667783
immedha <104690908+immedha@users.noreply.github.com>
2025-01-31 12:19:32 -0800

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,23 @@ We will be using it to create a simple chatbot interface: displaying the previou
 
 You need to learn just a couple of things first to use it: 
 (1) what is session state: https://docs.streamlit.io/develop/api-reference/caching-and-state/st.session_state
-(2) How to use the chat_message function for displaying the assistant/user messages in a chatbot interface: https://docs.streamlit.io/develop/api-reference/chat/st.chat_message
-(3) How to get user input (text_input function): https://docs.streamlit.io/develop/api-reference/widgets/st.text_input
+(2) How to use the chat_message and markdoown() functions for displaying the assistant/user messages in a chatbot interface: https://docs.streamlit.io/develop/api-reference/chat/st.chat_message
+(3) How to get user input (chat_input function): https://docs.streamlit.io/develop/api-reference/widgets/st.text_input
 
 NOTE: To run your streamlit app, run the coommand `streamlit run main.py` in your terminal.
 """
 
 import streamlit as st
-import openai
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
 
 # Here we load the OpenAI API key from the environment variable and set it
+load_dotenv()
 api_key = os.getenv("OPENAI_API_KEY")
 
 if not api_key:
     st.error("API key not found. Please set the OPENAI_API_KEY in the .env file.")
-else:
-    openai.api_key = api_key
 
 # TODO: Create an Open AI client object and set the API key
 
@@ -29,11 +28,15 @@ else:
 
 # TODO: Here we will set up all the Streamlit display elements. 
 # (1) Create a title for the streamlit page, 
-# (2) display the previous message history using streamlit's chat_message function, and 
-# (3) display an input element for the user's message
+# (2) display the previous message history using streamlit's chat_message() and markdown() functions, and 
+# (3) display an input element for the user's message using streamlit's chat_input() function
 
 # TODO: Here we will handle the user submitting the message and getting the AI response.
-# Remember that when calling the Open AI API, you need to include the entire message history in order for the AI to remember the conversation.
+# You need to check if the user submitted something in the input element and if so, 
+# (1) display the user's message using markdown()
+# (2) append the user's message to the session state
+# (3) generate the AI response using the Open AI API (make sure to include the entire message history so the AI has a memory of the conversation)
+# (4) display the AI response using markdown() and append it to the session state
 
-# TODO: Not only should you append the new AI response to the message history, but you also need to display the new respnse for now 
-# because Streamlit will only display the updated message history on the NEXT render of the page.
+# NOTE: We do steps 1 and 4 even though we are already appending it to the session state, because streamlit will only update the display 
+# automatically on the NEXT re-render.
